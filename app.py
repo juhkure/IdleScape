412bc67245303e994dd127fb2182667391368b3b
juhkure <johannes.pyykonen@helsinki.fi>
2022-11-15 22:58:43 +0200

    first non-working attempts with app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+db = SQLAlchemy(app)
+
+from database import init_db
+
+
+answer = input("Do you want to fill database? (y/n): ")
+if answer == "y":
+    db.session.execute("INSERT INTO activities (id, name) VALUES (1, 'fishing')")
+    db.session.commit()
+    # init_db.testi()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
