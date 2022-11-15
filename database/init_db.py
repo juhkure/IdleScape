c2a85c1f38452a54a653104eb361735f1478da78
juhkure <johannes.pyykonen@helsinki.fi>
2022-11-15 22:56:15 +0200

    added database directory with database init files

---
+++ database/init_db.py
@@ -0,0 +1,15 @@
+
+
+
+def testi():
+    print ("hei")
+    # add_activities()
+    
+
+def add_all():
+    print ("asd")
+
+
+# def add_activities():
+    # db.session.execute("INSERT INTO activities (id, name) VALUES (1, 'fishing')")
+
