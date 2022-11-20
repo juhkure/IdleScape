864723afc6217055cb83b17bf23d3b6951db0195
juhkure <johannes.pyykonen@helsinki.fi>
2022-11-20 14:01:41 +0200

    updated app commenting

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from os import path, getenv
 
 app = Flask(__name__)
 
-################################################################
+###################################################################################
 # Change this to:
 # 0 | Normal run from .env variables
 # 1 | Local testing with database creation/modification included
-# 2 | Local testing with instant run (no terminal configuration)
+# 2 | Local testing with existing database (with terminal configuration)
+# 3 | Local testing with existing database instant run (no terminal configuration)
 run_mode = 1
-################################################################
+###################################################################################
 
 
 import routes
