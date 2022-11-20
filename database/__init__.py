1fcdd3f890fe99be752a2528c47ced46dc6ca0bc
juhkure <johannes.pyykonen@helsinki.fi>
2022-11-20 12:27:35 +0200

    removed useless module for now

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,4 @@
 from .database_filler import fill_database, empty_tables
-from .new_user import create_user, insert_user_skills
 from .create_database import new_database, create_tables, user_password, database_user, database_name, delete_tables
 
 # from .database_filler import drop_tables
@@ -8,8 +7,6 @@ from .create_database import new_database, create_tables, user_password, databas
 __all__ = [
     'fill_database',
     'empty_tables'
-    'create_user',
-    'insert_user_skills'
     'new_database', 
     'create_tables', 
     'user_password',
